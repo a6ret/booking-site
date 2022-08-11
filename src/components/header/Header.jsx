9788afc51f12b4c81fa86b27c0708dfbf03d00bc
a6ret <89723087+a6ret@users.noreply.github.com>
2022-08-11 15:22:16 +0700

    Update

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import { FaBed } from 'react-icons/fa';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='headerList'>
+                <div className='headerListItem'>
+                    <FaBed />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
